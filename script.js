865f9e3c23e1171e73b2304af13ba6e0dbbc16e3
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-14 12:35:24 +0430

    init

--- script.js
+++ script.js
@@ -3,3 +3,10 @@
 console.log('hey i\'am here ' + 'hey')
 
 console.log(500 % 3)
+
+const initialApples = 4;
+const applesToBuy = 27;
+
+const totalApples = initialApples + applesToBuy;
+
+console.log(totalApples)
