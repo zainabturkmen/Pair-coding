c7385890d14920a38bb0fe26cb794c9a62be4be4
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-15 22:45:55 +0430

    init

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//JavaScript
+ffunction convertBinaryToDecimal() {
+    const binaryInput = document.getElementById("binaryInput").value;
+
+    // Validate binary input
+    if (/^[01]+$/.test(binaryInput)) {
+      const decimalEquivalent = parseInt(binaryInput, 2);
+      document.getElementById("result").innerText = `Decimal Equivalent: ${decimalEquivalent}`;
+    } else {
+      document.getElementById("result").innerText = 'Invalid Binary Input. Enter 0s and 1s only.';
+    }
+  }
