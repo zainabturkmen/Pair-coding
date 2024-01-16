2162544df62620553791011df148550e4095495e
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-16 22:23:02 +0430

    init

--- script.js
+++ script.js
@@ -1,26 +1,13 @@
-// function convertBinaryToDecimal() {
-//     const binaryInput = document.getElementById("binaryInput").value;
-
-//     // Validate binary input
-//     if (/^[01]+$/.test(binaryInput)) {
-//       const decimalEquivalent = parseInt(binaryInput, 2);
-//       document.getElementById("result").innerText = `Decimal Equivalent: ${decimalEquivalent}`;
-//     } else {
-//       document.getElementById("result").innerText = 'Invalid Binary Input. Enter 0s and 1s only.';
-//     }
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function convertBinaryToDecimal() {
+    const binaryInput = document.getElementById("binaryinput").value;
+  
+    // Validate binary input
+    if (/^[01]+$/.test(binaryInput)) {
+      const decimalEquivalent = parseInt(binaryInput, 2);
+      document.getElementById("result").innerText = `${decimalEquivalent}`;
+    } else {
+      document.getElementById("result").innerText =
+        "Invalid Binary Input. Enter 0s and 1s only.";
+    }
+  }
+  
