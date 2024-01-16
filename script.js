db30ab5bae5de4e5f961ac6da854a610dccac58f
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-16 17:02:00 +0430

    init

--- script.js
+++ script.js
@@ -9,13 +9,20 @@
 //       document.getElementById("result").innerText = 'Invalid Binary Input. Enter 0s and 1s only.';
 //     }
 //   }
-function four(){
-    return 4
+
+
+
+function log(){
+    console.log('hey there')
 }
 
-function add8(callback){
-   return callback() + 8
-}
-const result = add8(four)
+log()
 
+function sum(num1, num2){
+    return num1 + num2
+}
+
+const result = sum(2,6)
 console.log(result)
+
+
