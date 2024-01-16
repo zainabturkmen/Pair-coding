4c7df2c82efd9b2f738f3815d6cd24e3b51c5952
zainabturkmen <zainabturkmen7@gmail.com>
2024-01-16 12:24:57 +0430

    init

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function convertBinaryToDecimal() {
-    const binaryInput = document.getElementById("binaryInput").value;
+// function convertBinaryToDecimal() {
+//     const binaryInput = document.getElementById("binaryInput").value;
 
-    // Validate binary input
-    if (/^[01]+$/.test(binaryInput)) {
-      const decimalEquivalent = parseInt(binaryInput, 2);
-      document.getElementById("result").innerText = `Decimal Equivalent: ${decimalEquivalent}`;
-    } else {
-      document.getElementById("result").innerText = 'Invalid Binary Input. Enter 0s and 1s only.';
-    }
-  }
+//     // Validate binary input
+//     if (/^[01]+$/.test(binaryInput)) {
+//       const decimalEquivalent = parseInt(binaryInput, 2);
+//       document.getElementById("result").innerText = `Decimal Equivalent: ${decimalEquivalent}`;
+//     } else {
+//       document.getElementById("result").innerText = 'Invalid Binary Input. Enter 0s and 1s only.';
+//     }
+//   }
+
+
+const Hi = "hello world"
+
+function hayHi (){
+    console.log(Hi)
+}
+
+// console.log(hayHi)
+hayHi()
+
